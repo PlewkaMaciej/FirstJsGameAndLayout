dad1c5a83a9abc20df0d7bd21234f5ccdbf136c8
Maciej Plewka <plewka-maciej@wp.pl>
2021-07-24 22:09:02 +0200

    added stopWatch

--- budzik-layout/czas.js
+++ budzik-layout/czas.js
@@ -7,6 +7,7 @@ const timeNewYork = document.querySelector("#time-newyork");
 const timeSydney = document.querySelector("#time-sydney");
 const timeToronto = document.querySelector("#time-toronto");
 const timeTokio = document.querySelector("#time-tokio");
+console.log(timeSydney);
 const checkTime = (i) => {
   if (i < 10) {
     i = "0" + i;
@@ -16,12 +17,16 @@ const checkTime = (i) => {
 const getTime = (addedTime, city) => {
   let today = new Date();
   let hours = checkTime(today.getHours()) + addedTime;
-  if (hours > 24) {
-    hours = hours % 24;
+  hours = parseInt(hours);
+  if (hours > 23) {
+    hours = checkTime(hours) % 24;
+  }
+  if (hours < 0) {
+    hours = 24 + addedTime;
   }
   let minutes = checkTime(today.getMinutes());
   let seconds = checkTime(today.getSeconds());
-  let actualtime = hours + ":" + minutes + ":" + seconds;
+  let actualtime = checkTime(hours) + ":" + minutes + ":" + seconds;
   city.innerText = actualtime;
 };
 const setAllTimes = () => {
--- budzik-layout/stopwatch.js
+++ budzik-layout/stopwatch.js
@@ -1,19 +1,23 @@
 const start = document.querySelector(".start-button");
 const restart = document.querySelector(".reset-button");
 const timer = document.querySelector(".stop-watch-current-time");
-let stoptime = true;
-const timerCycle = () => {
-  let sec = 0;
-  let hr = 0;
-  let min = 0;
-  setInterval(() => {
-    sec = parseInt(sec);
-    min = parseInt(min);
-    hr = parseInt(hr);
+const newRound = document.querySelector(".new-button");
+const roundSection = document.querySelector(".roundSection");
+let isWorking = false;
+let sec = 0;
+let hr = 0;
+let min = 0;
+let numberOfRound=0;
+const checkTime = (i) => {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+};
+let stopwatch;
+const turnOnStopWatch = () => {
+  stopwatch = setInterval(() => {
     sec = sec + 1;
-    start.style.backgroundColor = "red";
-    start.innerText = "Stop";
-
     if (sec > 59) {
       sec = 0;
       min = min + 1;
@@ -22,26 +26,51 @@ const timerCycle = () => {
       min = 0;
       hr = hr + 1;
     }
-    if (sec < 10 || sec === 0) {
-      sec = "0" + sec;
-    }
-    if (min < 10 || min === 0) {
-      min = "0" + min;
-    }
-    if (hr < 10 || hr === 0) {
-      hr = "0" + hr;
-    }
-
-    timer.innerText = hr + ":" + min + ":" + sec;
+    timer.innerText =
+      checkTime(hr) + ":" + checkTime(min) + ":" + checkTime(sec);
   }, 1000);
 };
 
+const timerCycle = () => {
+  if (isWorking === false) {
+    isWorking = true;
+    turnOnStopWatch();
+    start.style.backgroundColor = "red";
+    start.innerText = "Stop";
+  } else {
+    isWorking = false;
+    clearInterval(stopwatch);
+    start.style.backgroundColor = "yellow";
+    start.innerText = "Start";
+  }
+};
 const resetTimer = () => {
   hr = 0;
   sec = 0;
   min = 0;
   timer.innerText = "00:00:00";
+  clearInterval(stopwatch);
+  start.style.backgroundColor = "yellow";
+  start.innerText = "Start";
+  remove()
 };
-
+const newRoundFunc = () => {
+  if(!isWorking){
+    return
+  }
+  let p = document.createElement("p");
+  numberOfRound++
+  p.innerText = `${numberOfRound} okrążenie w czasie: ${timer.innerText}`;
+  p.classList.add("timeRound");
+  roundSection.appendChild(p);
+};
+const remove=()=>{
+  let allTimes=document.querySelectorAll(".timeRound") 
+  allTimes.forEach((p)=>{
+    p.remove()
+    numberOfRound=0
+  })
+}
 start.addEventListener("click", timerCycle);
 restart.addEventListener("click", resetTimer);
+newRound.addEventListener("click", newRoundFunc);
